bdb322c0959b7828ce514a92327903c3858092b4
Connie Ip <connie.ip@cargosmart.com>
2018-11-06 17:09:33 +0800

    Implement Section1 practice-4.js

--- spec/section-1/practice-4.js
+++ spec/section-1/practice-4.js
@@ -1,5 +1,15 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+   var x;
+   var y;
+   var result = []
+   for(x in collectionA){
+     for(y in objectB.value){
+       if(collectionA[x].key == objectB.value[y]){
+         result.push(collectionA[x].key);
+       }
+     }
+   }
+   return result;
 }
