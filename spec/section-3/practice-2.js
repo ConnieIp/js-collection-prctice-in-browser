c40fcece06bfcb59e98e86189b8e799d8c1bb46f
Connie Ip <connie.ip@cargosmart.com>
2018-11-06 17:55:52 +0800

    Implement Section3 practice-2.js

--- spec/section-3/practice-2.js
+++ spec/section-3/practice-2.js
@@ -1,5 +1,15 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+   var x;
+   var y;
+   for(x in collectionA){
+     for(y in objectB.value){
+       if(collectionA[x].key == objectB.value[y]){
+         var minus=Math.floor(collectionA[x].count/3);
+         collectionA[x].count=collectionA[x].count-minus;
+       }
+     }
+   }
+   return collectionA;
 }
