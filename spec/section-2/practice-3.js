d97cc211887c2de07fda1f4053741a63bee080f2
Connie Ip <connie.ip@cargosmart.com>
2018-11-06 17:40:30 +0800

    Impplement Section2 practice-3.js

--- spec/section-2/practice-3.js
+++ spec/section-2/practice-3.js
@@ -1,5 +1,35 @@
 'use strict';
 
 function countSameElements(collection) {
-  return 'Implement the practice require, and begin changing code in this row';
+  var x;
+  var y;
+  var result = []
+  for (x in collection){
+    var exist = false;
+    var char;
+    var cnt;
+    if(collection[x].includes("-")){
+      char=collection[x].substring(0,collection[x].indexOf("-"));
+      cnt=parseInt(collection[x].substring(collection[x].indexOf("-")+1));
+    }else if(collection[x].includes(":")){
+      char=collection[x].substring(0,collection[x].indexOf(":"));
+      cnt=parseInt(collection[x].substring(collection[x].indexOf(":")+1));
+    }else if(collection[x].includes("[")&&collection[x].includes("]")){
+      char=collection[x].substring(0,collection[x].indexOf("["));
+      cnt=parseInt(collection[x].substring(collection[x].indexOf("[")+1,collection[x].indexOf("]")));
+    }else{
+      char=collection[x];
+      cnt=1;
+    }
+    for (y in result){
+      if(result[y].name==char){
+        result[y].summary=result[y].summary+cnt;
+        exist = true;
+      }
+    }
+    if(!exist){
+      result.push({name:char,summary:cnt});
+    }
+  }
+  return result;
 }
