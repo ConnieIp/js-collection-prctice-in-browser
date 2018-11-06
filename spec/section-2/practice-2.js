43f267e34d611c271b0f8f2b620204a8eedfd655
Connie Ip <connie.ip@cargosmart.com>
2018-11-06 17:31:11 +0800

    Implement Section2 practice-2.js

--- spec/section-2/practice-2.js
+++ spec/section-2/practice-2.js
@@ -1,5 +1,29 @@
 'use strict';
 
 function countSameElements(collection) {
-  return 'Implement the practice require, and begin changing code in this row';
+  var x;
+  var y;
+  var result = []
+  for (x in collection){
+    var exist = false;
+    var char;
+    var cnt;
+    if(collection[x].includes("-")){
+      char=collection[x].substring(0,collection[x].indexOf("-"));
+      cnt=parseInt(collection[x].substring(collection[x].indexOf("-")+1));
+    }else{
+      char=collection[x];
+      cnt=1;
+    }
+    for (y in result){
+      if(result[y].key==collection[x]){
+        result[y].count=result[y].count+1;
+        exist = true;
+      }
+    }
+    if(!exist){
+      result.push({key:char,count:cnt});
+    }
+  }
+  return result;
 }
