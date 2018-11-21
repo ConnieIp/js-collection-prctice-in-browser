12c9a34964c5fe03428ce330c2d2c1bcfabd1a41
Connie Ip <connie.ip@cargosmart.com>
2018-11-21 11:52:21 +0800

    pre-training w/o loop modification

--- spec/section-1/practice-1.js
+++ spec/section-1/practice-1.js
@@ -1,15 +1,20 @@
 'use strict';
 
+// function collectSameElements(collectionA, collectionB) {
+//   var x;
+//   var y;
+//   var result = []
+//   for(x in collectionA){
+//     for(y in collectionB){
+//       if(collectionA[x] == collectionB[y]){
+//         result.push(collectionA[x]);
+//       }
+//     }
+//   }
+//   return result;
+// }
+
+
 function collectSameElements(collectionA, collectionB) {
-  var x;
-  var y;
-  var result = []
-  for(x in collectionA){
-    for(y in collectionB){
-      if(collectionA[x] == collectionB[y]){
-        result.push(collectionA[x]);
-      }
-    }
-  }
-  return result;
-}
+    return collectionA.filter(a => collectionB.includes(a));
+};
--- spec/section-1/practice-4.js
+++ spec/section-1/practice-4.js
@@ -1,15 +1,20 @@
 'use strict';
 
+// function collectSameElements(collectionA, objectB) {
+//     var x;
+//     var y;
+//     var result = []
+//     for (x in collectionA) {
+//         for (y in objectB.value) {
+//             if (collectionA[x].key == objectB.value[y]) {
+//                 result.push(collectionA[x].key);
+//             }
+//         }
+//     }
+//     return result;
+// }
+
+
 function collectSameElements(collectionA, objectB) {
-   var x;
-   var y;
-   var result = []
-   for(x in collectionA){
-     for(y in objectB.value){
-       if(collectionA[x].key == objectB.value[y]){
-         result.push(collectionA[x].key);
-       }
-     }
-   }
-   return result;
-}
+    return collectionA.filter(a => objectB.value.includes(a.key)).map(x => x.key);
+};
